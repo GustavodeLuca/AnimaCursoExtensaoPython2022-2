c3a306d936f2c5a0976d768d2cfac8d4590a95a2
Gustavodeluca1 <116230263+GustavodeLuca@users.noreply.github.com>
2022-10-27 01:01:05 +0000

    Aprendemos sobre float

--- main.py
+++ main.py
@@ -1,34 +1,16 @@
-# comando input(): quero permitir que 
-# o usuário digite algo...
-nome = input("Digite seu nome: ")
-#pede a idade para o usuário "Qual sua idade?"
-idade = int(input("Digite sua idade: "))
+#Pede o nome do aluno e sua nota (de 0 a 10) e, se ele tirou nota 10, mostra "Você é bichão, mesmo..."
+nome = input("Informe seu nome: ")
+nota = float(input("Digite sua nota: "))
 
-#comando de saída..exibir na tela
-print(f"Boa noite, seu nome é {nome}")
-#exiba "Sua idade é ..."
-print("Sua idade é {}".format(idade))
-
-#e se eu quisesse mostrar o DOBRO da idade informada?
-dobro = idade * 2
-print("O dobro da idade informada é {}".format(dobro))
-
-#Estrutura condicional - o famoso "SE" (if)
-#Se a pessoa for maior de idade, mostre "Você é maior de idade, ótimo! Já pode beber ou dirigir"
-if idade >= 18:
-  print("Você é maior de idade, ótimo! Já pode beber ou dirigir")
-else:
-  print("Você é xóvem ainda, xóvem ainda...")
-
-#E se eu quisesse perguntar o gênero (M = Masculino e F = Feminino)
-#Mostre (...e você também precisa/precisou prestar o serviço militar obrigatório)
-genero = input("Informe o gênero? M = Masculino, F = Feminino e O = Outros: ")
-if idade >= 18 and genero == "M" or "m":
-  print("...e você também precisa/precisou prestar o serviço militar obrigatório")
-
+if (nota == 10):
+  print(f"{nome}, você é o bixão mesmo...")
+elif (nota >= 6 and nota < 10):
+  print(f"{nome}. bom trabalho")
+elif (nota > 10):
+  print(f"Digite um número entre 0 e 10")
+else: # é sempre automaticamente o que as duas condições não tratamento
+  print("Burro, não tirou dez...")
 
 
 
   
-
-print ("Vai ser executada de qualuqer jeito")
