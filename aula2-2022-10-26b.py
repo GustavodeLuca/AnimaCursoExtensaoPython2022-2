1705317d8b8ae5317d6dd899127a0511fa7b60d1
Gustavodeluca1 <116230263+GustavodeLuca@users.noreply.github.com>
2022-10-27 01:19:22 +0000

    Exemplo de laço

---
+++ aula2-2022-10-26b.py
@@ -0,0 +1,16 @@
+#Pede o nome do aluno e sua nota (de 0 a 10) e, se ele tirou nota 10, mostra "Você é bichão, mesmo..."
+nome = input("Informe seu nome: ")
+nota = float(input("Digite sua nota: "))
+
+if (nota == 10):
+  print(f"{nome}, você é o bixão mesmo...")
+elif (nota >= 6 and nota < 10):
+  print(f"{nome}. bom trabalho")
+elif (nota > 10):
+  print(f"Digite um número entre 0 e 10")
+else: # é sempre automaticamente o que as duas condições não tratamento
+  print("Burro, não tirou dez...")
+
+
+
+  
--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-#Pede o nome do aluno e sua nota (de 0 a 10) e, se ele tirou nota 10, mostra "Você é bichão, mesmo..."
-nome = input("Informe seu nome: ")
-nota = float(input("Digite sua nota: "))
+#Exemplo de laço
 
-if (nota == 10):
-  print(f"{nome}, você é o bixão mesmo...")
-elif (nota >= 6 and nota < 10):
-  print(f"{nome}. bom trabalho")
-elif (nota > 10):
-  print(f"Digite um número entre 0 e 10")
-else: # é sempre automaticamente o que as duas condições não tratamento
-  print("Burro, não tirou dez...")
+#Se eu quisesse exibir números de 1 a 10?
+print("Aqui mostramos de maneira 'rústica'")
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+print("6")
+print("7")
+print("8")
+print("9")
+print("10")
 
-
-
-  
+print("Aqui é do jeito certo...")
+numero = 1
+print(numero)
+numero = numero + 1
+print(numero)
