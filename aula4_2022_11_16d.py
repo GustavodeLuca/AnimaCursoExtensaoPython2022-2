49385f3c400efbc9f424d362c2acc098065d5aae
Gustavodeluca1 <116230263+GustavodeLuca@users.noreply.github.com>
2022-11-24 00:05:37 +0000

    Exemplos de consultas!

--- aula4_2022_11_16d.py
+++ aula4_2022_11_16d.py
@@ -3,8 +3,11 @@ import aula4_2022_11_16c as bd
 con, cur = bd.conectar()
 
 nome = input("Informe o nome do herói/vilão: ")
-nome_civil = input("Informe o nome civil do herói/vilão (sua identidade secreta): ")
+nome_civil = input(
+  "Informe o nome civil do herói/vilão (sua identidade secreta): ")
 tipo_numerico = input("Tecle 1 para Herói(na) ou 2 para Vilã(o): ")
+
+#Consulta aqui a tabela grupos e exibe na tela
 
 #Consulta para o valor máximo usado no banco
 sql = "SELECT MAX(pessoa_id)+1 FROM pessoas"
@@ -15,9 +18,12 @@ if tipo_numerico == "1":
   tipo = "Herói(na)"
 else:
   tipo = "Vilã(o)"
-  
+
 sql = f"INSERT INTO pessoas (pessoa_id, nome, nome_civil, tipo) VALUES ({pessoa_id}, '{nome}', '{nome_civil}', '{tipo}')"
 
 cur.execute(sql)
+
+#Insert pessoas_grupo
+#INSERT INTO pessoas_grupo (pessoa_id, grupo_id) VALUE (X, X)
 con.commit()
 con.close()
